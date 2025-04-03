ef54ad45eeae786a31eb0eb23a0e10f3af68a901
Ariadeath <konradsgraeff@gmail.com>
2025-04-03 07:50:24 -0700

    Added htmlnode test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 
 def main():
     text = "This is some anchor text"
@@ -6,5 +7,13 @@ def main():
     url = "https://www.boot.dev"
     example_node = TextNode(text, text_type, url)
     print(example_node)
+
+    props = {
+    "href": "https://www.google.com",
+    "target": "_blank",
+}
+    example_html = HTMLNode(None, None, None, props)
+    print(example_html)
+
 
 main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props1(self):
+        tag = "bob"
+        value = 1
+        children = None
+        props = "aaron"
+    
+    def test_props2(self):
+        tag = "carol"
+        value = 2
+        children = None
+        props = "chester"
+    
+    def test_props3(self):
+        tag = "john"
+        value = 1
+        children = None
+        props = "billy"
