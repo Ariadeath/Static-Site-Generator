96e8a590afc267bcd245cd50defa9f6177a297f3
Ariadeath <konradsgraeff@gmail.com>
2025-04-04 11:13:17 -0700

    Saved stopped location

--- src/split_img_and_link.py
+++ src/split_img_and_link.py
@@ -2,7 +2,13 @@ from extract_markdown import extract_markdown_links, extract_markdown_images
 from textnode import TextNode,TextType
 
 def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        
     pass
 
 def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        
     pass
